d1df93da3cb8425f8542267c96a7e6d198c48193
Neeraj <neeraj.sathian@gmail.com>
2020-10-17 21:08:42 +0200

    Update service-worker.js

--- footballpeek.com/service-worker.js
+++ footballpeek.com/service-worker.js
@@ -24,6 +24,11 @@ this.addEventListener('fetch', function (event) {
             .then(function (response) {
                 // Check that the status is OK
                 if (/^0|([123]\d\d)|(40[14567])|410$/.test(response.status)) {
+                    if (event.request.method === 'GET') {
+                        caches.open(cacheVersion).then(function (cache) {
+                            cache.put(event.request.clone(), response.clone());
+                        });
+                    }
 
                     return response.clone();
                 }
@@ -32,7 +37,17 @@ this.addEventListener('fetch', function (event) {
                 throw new Error('Bad response');
             })
             .catch(function (error) {
-                throw new Error('No Network!');
+                return caches.match(event.request).then(function (response) {
+                    if (response) {
+                        return response;
+                    }
+
+                    if (originalResponse) {
+                        return originalResponse;
+                    }
+
+                    throw error;
+                });
             })
     );
 });
