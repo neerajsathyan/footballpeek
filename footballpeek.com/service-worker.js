13bf49cc9798e518d3a306b3557a844f5b36144b
Neeraj <neeraj.sathian@gmail.com>
2020-10-23 01:54:50 +0200

    Update service-worker.js

--- footballpeek.com/service-worker.js
+++ footballpeek.com/service-worker.js
@@ -20,34 +20,20 @@ this.addEventListener('fetch', function (event) {
     let originalResponse;
 
     event.respondWith(
-        fetch(event.request.clone())
-            .then(function (response) {
-                // Check that the status is OK
-                if (/^0|([123]\d\d)|(40[14567])|410$/.test(response.status)) {
-                    if (event.request.method === 'GET') {
-                        caches.open(cacheVersion).then(function (cache) {
-                            cache.put(event.request.clone(), response.clone());
-                        });
-                    }
-
-                    return response.clone();
-                }
-
-                originalResponse = response;
-                throw new Error('Bad response');
-            })
-            .catch(function (error) {
-                return caches.match(event.request).then(function (response) {
-                    if (response) {
-                        return response;
-                    }
-
-                    if (originalResponse) {
-                        return originalResponse;
-                    }
-
-                    throw error;
-                });
-            })
+        caches.match(event.request.clone()).then(function (resp) {
+               return resp || fetch(event.request).then(function(response) {
+                   //No Cache, so hitting network..
+                   caches.open(cacheVersion).then(function (cache){
+                       cache.put(event.request.clone(), response);
+                   });
+                   return response.clone();
+               });
+        }).catch(function(error) {
+            //No cache no network 503 service unaivalable..
+            var myBlob = new Blob();
+            var init = { "status" : 503 , "statusText" : "Service Unaivailable!" };
+            var myResponse = new Response(myBlob,init);
+            return myResponse
+        })
     );
 });
